f89e198ceb5cf3032f6f5f182b090ad7c0916555
Тимур Мамедов <Tima_mamedov_1991@mail.ru>
2020-01-19 18:34:39 +0300

    WebDriverFactory

--- tests-junit/src/main/java/FirstTest.java
+++ tests-junit/src/main/java/FirstTest.java
@@ -3,29 +3,41 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 public class FirstTest {
-  private WebDriver driver;
+   WebDriver wd;
 
   private static final Logger logger = LogManager.getLogger(FirstTest.class);
 
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
+    wd = new ChromeDriver();
   }
 
   @After
   public void tearDown() {
-    driver.quit();
+    wd.quit();
   }
 
 
   @Test
   public void testFirstTest() {
     logger.info("Start test FirstTest!");
-    driver.get("https://otus.ru/");
+    WebDriverFactory();
+    wd.get("https://otus.ru/");
     logger.info("Stop test FirstTest!");
 
+  }
+
+  private void WebDriverFactory() {
+    String browser = BrowserType.FIREFOX;
+    if (browser.equals(BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    }
   }
 
 
